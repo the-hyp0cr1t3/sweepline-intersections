bcf0957a878793e6c79568103e787b08080991d1
the-hyp0cr1t3 <hriday.gajulapalli@gmail.com>
2022-04-07 20:14:42 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-# Run as
-# $ build.sh [release|debug]
-
-cmake -DCMAKE_BUILD_TYPE=${1:-release} -S . -B build
-cmake --build build
-
-
-# cmake -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -S . -B build
